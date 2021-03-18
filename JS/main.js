20b02ffc6c58772ebcc03457c56bc2c90f14acc8
moudikhalid96 <moudi_khalid96@hotmail.com>
2021-03-18 20:51:14 +0300

    project completed

--- JS/main.js
+++ JS/main.js
@@ -1,114 +1,103 @@
 $(document).ready(
-    // When click on the first picture
-    $('#pic1').click(function(){
-        $('#pictures').hide('slow');
-        $('#puzzle').show('slow');
-        $('.square1, .square2, .square3, .square4, .square5, .square6, .square7, .square8, square9').css({'background-image': 'url(image/balloon.png)', 'width': '10em', 'height': '10em'});
-        // shuffle();
-    }),
-
-    // When click on the second picture
-    $('#pic2').click(function(){
-        $('#pictures').hide('slow');
-        $('#puzzle').show('slow');
-        $('.square1, .square2, .square3, .square4, .square5, .square6, .square7, .square8, square9').css({'background-image': 'url(image/flower.png)', 'width': '10em', 'height': '10em'});
-        // shuffle();
-    }),
-
-    // When click on the third picture
-    $('#pic3').click(function(){
-        $('#pictures').hide('slow');
-        $('#puzzle').show('slow');
-        $('.square1, .square2, .square3, .square4, .square5, .square6, .square7, .square8, square9').css({'background-image': 'url(image/clock.jpg)', 'width': '10em', 'height': '10em'});
-        // shuffle();
-    }),
-
     // When user want to change the picture
-    $('#changePicture').click(function(){
+    $('#changePicture').click(function () {
         $('#puzzle').hide('slow');
         $('#pictures').show('slow');
+        reset();
     })
 )
 
+$('li').click(function () {
+    $(this).each(function () {
+        switch (this.id) {
+            case 'pic1':
+                $('.square1, .square2, .square3, .square4, .square5, .square6, .square7, .square8').css({ 'background-image': 'url(image/balloon.png)' })
+                break;
+
+            case 'pic2':
+                $('.square1, .square2, .square3, .square4, .square5, .square6, .square7, .square8').css({ 'background-image': 'url(image/flower.png)' })
+                break;
+            case 'pic3':
+                $('.square1, .square2, .square3, .square4, .square5, .square6, .square7, .square8').css({ 'background-image': 'url(image/clock.jpg)' })
+                break;
+        }
+
+        $('#pictures').hide('slow');
+        $('#puzzle').show('slow');
+    })
+})
+
 // To shuffle/mix the squares on the puzzle
-function shuffle(){
-    for (let row=1 ; row<=3 ; row++) { //For each row in puzzle
-        for (let column=1 ; column<=3 ; column++) { //For each column in that row in puzzle
-        
-            let randomRow = parseInt(Math.random() * 10); //random row from 1 to 3
-            //in case row is grater than 3 or equals to 0
-            while(randomRow > 3 || randomRow == 0){
-                randomRow = parseInt(Math.random() * 10); 
-            }
+function shuffle() {
+    for (let row = 1; row <= 3; row++) { //For each row in puzzle
+        for (let column = 1; column <= 3; column++) { //For each column in that row in puzzle
 
-            let randomColumn = parseInt(Math.random() * 10); //random column from 1 to 3
-            //in case column is grater than 3 or equals to 0
-            while(randomColumn > 3 || randomColumn == 0){
-                randomColumn = parseInt(Math.random() * 10);
-            }
+            let randomRow = parseInt(Math.random() * 3) + 1; //random row from 1 to 3
+            let randomColumn = parseInt(Math.random() * 3) + 1; //random column from 1 to 3
 
-        
             //swipe up the two squares in a puzzle
-            squareSwiping('row' + row + 'column' + column, 'row' + randomRow + 'column' + randomColumn); 
+            squareSwiping('row' + row + 'column' + column, 'row' + randomRow + 'column' + randomColumn);
 
-        }//end for column
-    }//end for row 
+        }
+    }
 
-}//end of shuffle function
+}
 
-
-
+//swipe up the two squares in a puzzle (called inside function `shuffle` and `moveSquare`)
 function squareSwiping(square1, square2) {
     //Square1 has class and picture of 2, and Square2 has class and picture of 1
 
     //swip image
-    let template = document.getElementById(square1).style.backgroundImage;
-    document.getElementById(square1).style.backgroundImage = document.getElementById(square2).style.backgroundImage;
-    document.getElementById(square2).style.backgroundImage = template;
+    let template = $(`#${square1}`).css("background-image");
+    $(`#${square1}`).css("background-image", $(`#${square2}`).css("background-image"));
+    $(`#${square2}`).css("background-image", template);
+
+
 
     //swip position (class name)
-    let template2 = document.getElementById(square1).className;
-    document.getElementById(square1).className = document.getElementById(square2).className;
-    document.getElementById(square2).className = template2;
-    
+    let template2 = $(`#${square1}`).attr('class');
+    $(`#${square1}`).attr('class', $(`#${square2}`).attr('class'));
+    $(`#${square2}`).attr('class', template2);
+
 }//end of squareSwiping function
 
 
 //this function is to move squares from point to other
 function moveSquare(row, column) {
-    let square = document.getElementById('row' + row + 'column' + column).className;
+    let square = $(`#row${row}column${column}`).attr('class');
 
     //if user clicks on square9 .. nothing will happen
-    if (square != 'square9') { 
+    // unless it's not the square with class `square9`
+    if (square != 'square9') {
 
         //in case white square (square9) on right
         if (column < 3) {
-            if ( document.getElementById('row' + row + 'column' + (column+1)).className == 'square9') {
-                squareSwiping('row' + row + 'column' + column, 'row' + row + 'column' + (column+1));
+            if ($(`#row${row}column${column + 1}`).attr('class') == 'square9') {
+                squareSwiping('row' + row + 'column' + column, 'row' + row + 'column' + (column + 1));
                 return;
             }
         }
 
         //in case white square (square9) on left
         if (column > 1) {
-            if ( document.getElementById('row' + row + 'column' + (column-1)).className == 'square9') {
-                squareSwiping('row' + row + 'column' + column, 'row' + row + 'column' + (column-1));
+            if ($(`#row${row}column${column - 1}`).attr('class') == 'square9') {
+                squareSwiping('row' + row + 'column' + column, 'row' + row + 'column' + (column - 1));
                 return;
             }
         }
 
         //in case white square (square9) on bottom
         if (row < 3) {
-            if ( document.getElementById('row' + (row+1) + 'column' + column).className == 'square9') {
-                squareSwiping('row' + row + 'column' + column, 'row' + (row+1) + 'column' + column);
+            if ($(`#row${row + 1}column${column}`).attr('class') == 'square9') {
+                squareSwiping('row' + row + 'column' + column, 'row' + (row + 1) + 'column' + column);
                 return;
             }
         }
-        
+
         //in case white square (square9) on top
         if (row > 1) {
-            if ( document.getElementById('row' + (row-1) + 'column' + column).className == 'square9') {
-                squareSwiping('row' + row + 'column' + column, 'row' + (row-1) + 'column' + column);
+            if ($(`#row${row - 1}column${column}`).attr('class') == 'square9') {
+                squareSwiping('row' + row + 'column' + column, 'row' + (row - 1) + 'column' + column);
                 return;
             }
         }
